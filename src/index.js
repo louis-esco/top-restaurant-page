9e9d8a93ac151cbf9f8ae953ae1ee75311e55568
louis-esco <louis.escoffier.bagrel@gmail.com>
2024-04-19 15:47:09 +0300

    Display of homepage and eventlistener on btn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+import displayHome from './pages/home/home.js';
+
+function eventListeners() {
+    const navBtns = document.querySelectorAll('nav button');
+    navBtns[0].addEventListener('click', displayHome);
+}
+
+eventListeners();
+displayHome();
